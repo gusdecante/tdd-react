0a288fef9e751e8307651f9b9d491c201ed68767
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-05-21 15:38:00 -0300

    refactor: removing the default css configurantion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import SignUpPage from "./pages/SignUpPage";
 
 function App() {
-  return <SignUpPage />;
+  return (
+    <div className="container">
+      <SignUpPage />
+    </div>
+  );
 }
 
 export default App;
