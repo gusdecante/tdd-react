e7a69860d8e607d3e0547738ed7330ecd9dffd23
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-05-21 16:28:01 -0300

    chore: updating the styling for SignUpPage

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -43,20 +43,66 @@ class SignUp extends Component {
       enabled = password !== passwordRepeat;
     }
     return (
-      <div>
-        <form>
-          <h1>SignUp</h1>
-          <label htmlFor="username"> Username</label>
-          <input id="username" onChange={this.onChange} />
-          <label htmlFor="email"> E-mail</label>
-          <input id="email" onChange={this.onChange} />
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" onChange={this.onChange} />
-          <label htmlFor="passwordRepeat">Password Repeat</label>
-          <input type="password" id="passwordRepeat" onChange={this.onChange} />
-          <button disabled={enabled} onClick={this.submit}>
-            Sign Up
-          </button>
+      <div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+        <form className="card mt-5">
+          <div className="card-header">
+            <h1 className="text-center">SignUp</h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                {" "}
+                Username
+              </label>
+              <input
+                id="username"
+                onChange={this.onChange}
+                className="form-control"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                {" "}
+                E-mail
+              </label>
+              <input
+                id="email"
+                onChange={this.onChange}
+                className="form-control"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                onChange={this.onChange}
+                className="form-control"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordRepeat" className="form-label">
+                Password Repeat
+              </label>
+              <input
+                type="password"
+                id="passwordRepeat"
+                onChange={this.onChange}
+                className="form-control"
+              />
+            </div>
+            <div className="text-center">
+              <button
+                className="btn btn-primary"
+                disabled={enabled}
+                onClick={this.submit}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     );
