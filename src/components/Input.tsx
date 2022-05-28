648f0bbd29dbe8d4df124adbfa52ecf20281a5ca
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-05-27 23:32:28 -0300

    feat: has invalid-feedback class for span when help is set

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,27 @@
 import React, { ChangeEvent } from "react";
 
 type InputProps = {
-  id: string;
-  label: string;
-  onChange(event: ChangeEvent<HTMLInputElement>): void;
+  id?: string;
+  label?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
   help?: string;
 };
 
 const Input: React.FC<InputProps> = ({ id, label, onChange, help }) => {
+  let inputClass = "form-control";
+  if (help) {
+    inputClass += " is-invalid";
+  }
+
   return (
     <div className="mb-3">
       <label htmlFor={id} className="form-label">
         {label}
       </label>
-      <input id={id} onChange={onChange} className="form-control" />
-      <span>{help}</span>
+      <input id={id} onChange={onChange} className={inputClass} />
+      <span data-testid="help-message" className="invalid-feedback">
+        {help}
+      </span>
     </div>
   );
 };
