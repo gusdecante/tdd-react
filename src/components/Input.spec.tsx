e72db493a7f5f17a38d14b88a96606da90e55fc1
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-05-27 23:34:02 -0300

    test: has invalid-feedback class for span when help is set

---
+++ src/components/Input.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import Input from "./Input";
+
+it("has is-invalid class for input when help is set", () => {
+  render(<Input help="Error message" />);
+
+  const input = screen.getByRole("textbox");
+
+  expect(input.classList).toContain("is-invalid");
+});
+
+it("has invalid-feedback class for span when help is set", () => {
+  render(<Input help="Error message" />);
+
+  const help = screen.getByTestId("help-message");
+
+  expect(help.classList).toContain("invalid-feedback");
+});
+
+it("does not have is-invalid class for input when help is not set", () => {
+  render(<Input />);
+
+  const input = screen.getByRole("textbox");
+
+  expect(input.classList).not.toContain("is-invalid");
+});
