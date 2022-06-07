a30fbcc6c26e20a64ec966fc8c5c374cede02df5
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-07 20:56:10 -0300

    feat: rendering pages conditionally

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -78,7 +78,10 @@ class SignUpPage extends Component<WithTranslation> {
     let passwordMismatch =
       password !== passwordRepeat ? t("passwordMismatchValidation") : "";
     return (
-      <div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+      <div
+        className="col-lg-6 offset-lg-3 col-md-8 offset-md-2"
+        data-testid="signup-page"
+      >
         {!signUpSuccess && (
           <form className="card mt-5" data-testid="form-sign-up">
             <div className="card-header">
