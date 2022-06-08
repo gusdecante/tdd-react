efbbd69b94567c70cac490c24a58534347461697
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-08 00:02:02 -0300

    feat: adding links to the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
+import { MouseEvent, useState } from "react";
 import SignUpPage from "./pages/SignUpPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import UserPage from "./pages/UserPage";
-
 import LanguageSelector from "./components/LanguageSelector";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation();
+  const [path, setPath] = useState<string>(window.location.pathname);
+
+  const onClickLink = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    const el = event.target as HTMLAnchorElement;
+    const path = el.getAttribute("href") as string;
+    window.history.pushState({}, "", path);
+    setPath(path);
+  };
+
   return (
     <div className="container">
-      {window.location.pathname === "/" && <HomePage />}
-      {window.location.pathname === "/signup" && <SignUpPage />}
-      {window.location.pathname === "/login" && <LoginPage />}
-      {window.location.pathname.startsWith("/user/") && <UserPage />}
+      <div>
+        <a href="/" title="Home" onClick={onClickLink}>
+          Hoaxify
+        </a>
+        <a href="/signup" onClick={onClickLink}>
+          {t("signUp")}
+        </a>
+        <a href="/login" onClick={onClickLink}>
+          Login
+        </a>
+      </div>
+      {path === "/" && <HomePage />}
+      {path === "/signup" && <SignUpPage />}
+      {path === "/login" && <LoginPage />}
+      {path.startsWith("/user/") && <UserPage />}
       <LanguageSelector />
     </div>
   );
