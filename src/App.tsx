90ed326305cb4eb36525b8c73368783e73b5f823
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-08 23:00:32 -0300

    chore: styling the navbar and correcting the redirect

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import UserPage from "./pages/UserPage";
 import LanguageSelector from "./components/LanguageSelector";
 import { useTranslation } from "react-i18next";
+import logo from "./assets/hoaxify.png";
 
 function App() {
   const { t } = useTranslation();
@@ -12,31 +13,43 @@ function App() {
 
   const onClickLink = (event: MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
-    const el = event.target as HTMLAnchorElement;
+    const el = event.currentTarget as HTMLAnchorElement;
     const path = el.getAttribute("href") as string;
     window.history.pushState({}, "", path);
     setPath(path);
   };
 
   return (
-    <div className="container">
-      <div>
-        <a href="/" title="Home" onClick={onClickLink}>
-          Hoaxify
-        </a>
-        <a href="/signup" onClick={onClickLink}>
-          {t("signUp")}
-        </a>
-        <a href="/login" onClick={onClickLink}>
-          Login
-        </a>
+    <>
+      <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
+        <div className="container">
+          <a
+            className="navbar-brand"
+            href="/"
+            title="Home"
+            onClick={onClickLink}
+          >
+            <img src={logo} alt="Hoaxify" width="60" />
+            Hoaxify
+          </a>
+          <ul className="navbar-nav">
+            <a className="nav-link" href="/signup" onClick={onClickLink}>
+              {t("signUp")}
+            </a>
+            <a className="nav-link" href="/login" onClick={onClickLink}>
+              Login
+            </a>
+          </ul>
+        </div>
+      </nav>
+      <div className="container">
+        {path === "/" && <HomePage />}
+        {path === "/signup" && <SignUpPage />}
+        {path === "/login" && <LoginPage />}
+        {path.startsWith("/user/") && <UserPage />}
+        <LanguageSelector />
       </div>
-      {path === "/" && <HomePage />}
-      {path === "/signup" && <SignUpPage />}
-      {path === "/login" && <LoginPage />}
-      {path.startsWith("/user/") && <UserPage />}
-      <LanguageSelector />
-    </div>
+    </>
   );
 }
 
