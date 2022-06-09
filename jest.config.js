23dbed3e493a1dd8d08b4f5bfc4ac3c4cc45bed6
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-08 22:59:40 -0300

    fix: configuring jest to accept images

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,8 @@ module.exports = {
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/core/mocks/fileMock.ts",
   },
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 };
