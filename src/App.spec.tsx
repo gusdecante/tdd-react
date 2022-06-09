caeb20a6f2a50e89d7ec5a22e75ced8cb2e15314
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-08 23:01:25 -0300

    chore: removing unused import

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import useEvent from "@testing-library/user-event";
 import userEvent from "@testing-library/user-event";
 
 describe("Routing", () => {
