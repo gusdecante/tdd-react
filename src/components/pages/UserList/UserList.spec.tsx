fdd50576d90d03c66011353c426b3eb0b7f30cfd
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-07-19 20:53:08 -0300

    test: adding page and size to the list of users api call

--- src/components/pages/UserList/UserList.spec.tsx
+++ src/components/pages/UserList/UserList.spec.tsx
@@ -3,35 +3,78 @@ import { UserList } from "./";
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 
-const page1 = {
-  content: [
-    {
-      id: 1,
-      username: "user1",
-      email: "user1@mail.com",
-      image: null,
-    },
-    {
-      id: 2,
-      username: "user2",
-      email: "user2@mail.com",
-      image: null,
-    },
-    {
-      id: 3,
-      username: "user3",
-      email: "user3@mail.com",
-      image: null,
-    },
-  ],
-  page: 0,
-  size: 3,
-  totalPages: 9,
+const users = [
+  {
+    id: 1,
+    username: "user1",
+    email: "user1@mail.com",
+    image: null,
+  },
+  {
+    id: 2,
+    username: "user2",
+    email: "user2@mail.com",
+    image: null,
+  },
+  {
+    id: 3,
+    username: "user3",
+    email: "user3@mail.com",
+    image: null,
+  },
+  {
+    id: 4,
+    username: "user4",
+    email: "user4@mail.com",
+    image: null,
+  },
+  {
+    id: 5,
+    username: "user5",
+    email: "user5@mail.com",
+    image: null,
+  },
+  {
+    id: 6,
+    username: "user6",
+    email: "user6@mail.com",
+    image: null,
+  },
+  {
+    id: 7,
+    username: "user7",
+    email: "user7@mail.com",
+    image: null,
+  },
+];
+
+const getPage = (page: number, size: number) => {
+  let start = page * size;
+  let end = start + size;
+  const totalPages = Math.ceil(users.length / size);
+
+  return {
+    content: users.slice(start, end),
+    page,
+    size,
+    totalPages,
+  };
 };
 
 const server = setupServer(
   rest.get("/api/1.0/users", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(page1));
+    let page = Number.parseInt(req.url.searchParams.get("page") as string);
+    let size = Number.parseInt(req.url.searchParams.get("size") as string);
+
+    if (Number.isNaN(page)) {
+      page = 0;
+    }
+
+    if (Number.isNaN(size)) {
+      size = 5;
+    }
+
+    return res(ctx.status(200), ctx.json(getPage(page, size)));
   })
 );
 
