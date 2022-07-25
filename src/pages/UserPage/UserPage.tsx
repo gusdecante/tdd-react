7c0edc6e66d4b139cc668674c3dbd3eea4b6eda3
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-07-25 20:42:50 -0300

    feat: adding spinner to the user page while the api call is in progress

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -3,22 +3,33 @@ import { getUsersById } from "../../core/api/apiCalls";
 import { useParams } from "react-router-dom";
 import { UserData } from "../../components/pages/UserList/UserList";
 import { ProfileCard } from "../../components/pages/ProfileCard";
+import { Alert, Spinner } from "../../components";
 
 export const UserPage = () => {
   const [user, setUser] = useState<UserData>();
+  const [pendingApiCall, setPendingApiCall] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
     const handleGetUserById = async () => {
+      setPendingApiCall(true);
       try {
         const response = await getUsersById(Number(id));
         setUser(response.data);
       } catch (error) {}
+      setPendingApiCall(false);
     };
     handleGetUserById();
   }, []);
 
   return (
-    <div data-testid="user-page">{user && <ProfileCard user={user} />}</div>
+    <div data-testid="user-page">
+      {!pendingApiCall && user && <ProfileCard user={user} />}
+      {pendingApiCall && (
+        <Alert type="secondary" center>
+          <Spinner size="big" />
+        </Alert>
+      )}
+    </div>
   );
 };
