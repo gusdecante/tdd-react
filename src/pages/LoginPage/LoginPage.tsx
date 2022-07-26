a9d60cf3b7db0b84c252ae3e0bdc5c606f288474
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-07-26 20:47:49 -0300

    chore: enable the login button when the inputs are filled

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { Input } from "../../components";
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  let disabled = !(email && password);
+
   return (
     <div
       className="col-lg-6 offset-lg-3 col-md-8 offset-md-2"
@@ -11,10 +17,19 @@ export const LoginPage = () => {
           <h1 className="text-center">Login</h1>
         </div>
         <div className="card-body">
-          <Input id="email" label="E-mail" />
-          <Input id="password" label="Password" type="password" />
+          <Input
+            id="email"
+            label="E-mail"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
           <div className="text-center">
-            <button className="btn btn-primary" disabled>
+            <button className="btn btn-primary" disabled={disabled}>
               Login
             </button>
           </div>
