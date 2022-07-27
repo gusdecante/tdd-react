80a4e492413d9bd594d4964f8e141749227c65a7
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-07-26 21:39:34 -0300

    test: send email and password to the backend after clicking the login button

--- src/pages/LoginPage/LoginPage.spec.tsx
+++ src/pages/LoginPage/LoginPage.spec.tsx
@@ -1,7 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { LoginPage } from "./index";
-import UserEvent from "@testing-library/user-event";
 import userEvent from "@testing-library/user-event";
+import { setupServer } from "msw/node";
+import { rest, DefaultBodyType } from "msw";
+
+let requestBody: DefaultBodyType;
+let count = 0;
+
+const server = setupServer(
+  rest.post("/api/1.0/auth", (req, res, ctx) => {
+    count += 1;
+    requestBody = req.body;
+    return res(ctx.status(404));
+  })
+);
+
+beforeEach(() => {
+  count = 0;
+  server.resetHandlers();
+});
+
+beforeAll(() => server.listen());
+
+afterAll(() => server.close());
 
 describe("Login Page", () => {
   describe("Layout", () => {
@@ -40,7 +65,8 @@ describe("Login Page", () => {
     });
   });
   describe("Interactions", () => {
-    it("enables the button when email and password inputs are filled", () => {
+    let button: HTMLButtonElement;
+    const setup = () => {
       render(<LoginPage />);
       const emailInput = screen.getByLabelText("E-mail");
       const passwordInput = screen.getByLabelText("Password");
@@ -48,10 +74,39 @@ describe("Login Page", () => {
       userEvent.type(emailInput, "user100@mail.com");
       userEvent.type(passwordInput, "P4ssword");
 
-      const button = screen.queryByRole("button", {
+      button = screen.queryByRole("button", {
         name: "Login",
       }) as HTMLButtonElement;
+    };
+    it("enables the button when email and password inputs are filled", () => {
+      setup();
       expect(button.disabled).toBe(false);
+    });
+
+    it("displays spinner during api call", async () => {
+      setup();
+      expect(screen.queryByRole("status")).not.toBeInTheDocument();
+      userEvent.click(button);
+      const spinner = screen.getByRole("status");
+      await waitForElementToBeRemoved(spinner);
+    });
+    it("sends email and password to backend after clicking the button", async () => {
+      setup();
+      userEvent.click(button);
+      const spinner = screen.getByRole("status");
+      await waitForElementToBeRemoved(spinner);
+      expect(requestBody).toEqual({
+        email: "user100@mail.com",
+        password: "P4ssword",
+      });
+    });
+    it("disables the button when there is an api call", async () => {
+      setup();
+      userEvent.click(button);
+      userEvent.click(button);
+      const spinner = screen.getByRole("status");
+      await waitForElementToBeRemoved(spinner);
+      expect(count).toEqual(1);
     });
   });
 });
